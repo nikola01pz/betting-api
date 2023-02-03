4b846f80a13301d16397b8d2610fffc8864a6f65
Nikola Istvanic <nikola01pz@gmail.com>
2023-02-03 17:57:54 +0100

    implement user login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Login() {
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/login',{
+        user, 
+        password,
+      });
+      setMessage('Successful login');
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+      setMessage('Wrong user or password');
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  }
+
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={handleSubmit}>
       <label>
         User:
-        <input type="text" name="username" placeholder="Enter username or email" />
+        <input type="text" name="username" placeholder="Enter username or email" value={user} onChange={(e) => setUser(e.target.value)}/>
       </label>
       <br />
       <label>
         Password:
-        <input type="password" name="password" placeholder="Enter password" />
+        <input type="password" name="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)}/>
       </label>
       <br />
       <input className="log-button" type="submit" value="Login" />
+      {message && <div className="submit-message">{message}</div>}
     </form>
   );
-
 }
