19cc9bf863719e669b685dc6e9b13c8c85126c46
Nikola Istvanic <nikola01pz@gmail.com>
2023-06-13 21:40:12 +0200

    add user info when logged in and logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,14 @@ export default function Login() {
   const [user, setUser] = useState("")
   const [password, setPassword] = useState("")
   const [message, setMessage] = useState("")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loggedUser, setLoggedUser] = useState({
+    balance: 0,
+    email: "",
+    first_name: "",
+    last_name: "",
+    username: "",
+  })
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -14,32 +22,59 @@ export default function Login() {
         user, 
         password,
       })
-      setMessage("successful")
-      setTimeout(() => {
-        setMessage("")
-      }, 3000)
+      const {balance, email, first_name, last_name, username} = response.data
+      setLoggedUser({balance, email, first_name, last_name, username})
+      setIsLoggedIn(true)
       console.log(response)
     } catch(error) {
       console.error(error)
       setMessage("failed");
       setTimeout(() => {
         setMessage("")
-      }, 3000)
+      }, 2000)
     }
   }
 
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  }
+
+
   return (
     <>
-    <form className="login-form" onSubmit={handleLogin}>
-      <label>
-        <input className="login-input" type="text" name="username" placeholder="username or email" value={user} 
-          onChange={(e) => setUser(e.target.value)}/>
-        <input  className="login-input" type="password" name="password" placeholder="password" value={password} 
-          onChange={(e) => setPassword(e.target.value)}/>
-      </label>
-      <input className="log-button" type="submit" value="Login" />
-      {message && <div className="submit-message">{message}</div>}
-    </form>
+      {isLoggedIn ? (
+        <>
+          <div className="submit-message">{message}</div>
+          <div className="loggedUser-firstname">
+            {loggedUser.first_name && (
+              <span>Welcome {loggedUser.first_name}!</span>
+            )}
+          </div>
+          <div className="loggedUser-balance">
+            {loggedUser.balance && (
+              <span>Balance: €{loggedUser.balance}</span>
+            )}
+            <button className="add-balance">Deposit</button>
+          </div>
+          
+          <button className="log-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <form className="login-form" onSubmit={handleLogin}>
+            <label>
+              <input className="login-input" type="text" name="username" placeholder="username or email" value={user} 
+                onChange={(e) => setUser(e.target.value)}/>
+              <input  className="login-input" type="password" name="password" placeholder="password" value={password} 
+                onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <input className="log-button" type="submit" value="Login" />
+            {message && <div className="submit-message">{message}</div>}
+          </form>
+        </>
+      )}
     </>
   )
 }
